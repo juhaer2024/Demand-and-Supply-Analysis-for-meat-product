15ba2c63a0934a84916d16fd8e1df952e8f01d02
ramisa08 <ramisakarim86@gmail.com>
2025-04-30 20:33:52 +0600

    Add files via upload

---
+++ update_demand.php
@@ -0,0 +1,22 @@
+<?php
+include 'db.php';
+
+$id = $_POST['demand_id'];
+$year = $_POST['year'];
+$region = $_POST['region'];
+$demand_tons = $_POST['demand_tons'];
+$pattern = $_POST['consumption_pattern'];
+$elasticity = $_POST['price_elasticity'];
+$product_id = $_POST['product_id'];
+
+$sql = "UPDATE demand 
+        SET Year='$year', Region='$region', DemandInTons='$demand_tons', 
+            ConsumptionPattern='$pattern', PriceElasticity='$elasticity', ProductID='$product_id'
+        WHERE DemandID = $id";
+
+if ($conn->query($sql)) {
+    header("Location: demand.php");
+} else {
+    echo "Error: " . $conn->error;
+}
+?>
